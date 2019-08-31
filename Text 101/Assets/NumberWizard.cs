873dad8584076f77627b04a6b2a6b3ba47e84f0a
Paul Jeremy Fontanilla <paujfont@outlook.com>
2019-08-31 10:32:44 -0400

    Bug fixed - Can reach upper limits.
    Bug from initial commit fixed via Line 21.

--- Text 101/Assets/NumberWizard.cs
+++ Text 101/Assets/NumberWizard.cs
@@ -18,6 +18,7 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Press the 'Up Arrow' if your number is higher than my guess.");
         Debug.Log("Press the 'Down Arrow' if your number is lower than my guess.");
         Debug.Log("Press the 'Return Key' if my guess is correct!");
+        max = max + 1
         guess = (max + min) / 2;
         Debug.Log("My Guess is " + guess);
     }
@@ -46,6 +47,7 @@ public class NumberWizard : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Return))
         {
+            Debug.Log("Your number is " + guess + " !");
             Debug.Log("We did it!!");
         }
     }
